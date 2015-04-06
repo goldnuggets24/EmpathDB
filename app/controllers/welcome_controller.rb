4c58a42d69ef8239d6da913fa6d686079e8b7d1d
Mike Levine <mike.levine@gmail.com>
2015-04-06 17:12:44 -0400

    some style changes

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -8,12 +8,17 @@ class WelcomeController < ApplicationController
 		elsif params[:value] == "4"
 			@users = User.find(4)
 		else
-			@users = User.last
+			@users = User.first
 		end
 		@hash = Gmaps4rails.build_markers(@users) do |user, marker|
 		 	marker.lat user.latitude
   			marker.lng user.longitude
   			marker.infowindow user.title
+  			marker.picture({
+                  :url => "http://megaicons.net/static/img/icons_sizes/15/534/32/map-marker-ball-left-pink-icon.png",
+                  :width   => 32,
+                  :height  => 32
+                 })
 		end
 		if params[:value] == "2"
 			@picture = ActionController::Base.helpers.asset_path("2.png")
